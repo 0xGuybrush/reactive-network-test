4faadfa294805854c0ab34a6aea6b13d874529a8
Dave Moloney <dave@native.io>
2020-04-07 14:45:51 +0100

    Simplifying example

--- app/src/main/java/ie/davidmoloney/reactivenetworktest/BaseActivity.java
+++ app/src/main/java/ie/davidmoloney/reactivenetworktest/BaseActivity.java
@@ -2,18 +2,18 @@ package ie.davidmoloney.reactivenetworktest;
 
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
+import com.github.pwittchen.reactivenetwork.library.rx2.ReactiveNetwork;
+import com.github.pwittchen.reactivenetwork.library.rx2.internet.observing.InternetObservingSettings;
+import com.github.pwittchen.reactivenetwork.library.rx2.internet.observing.strategy.SocketInternetObservingStrategy;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
@@ -21,10 +21,12 @@ import io.reactivex.schedulers.Schedulers;
 
 public abstract class BaseActivity extends AppCompatActivity {
 
-    private InternetStatus internetStatus;
     private CompositeDisposable compositeDisposable = new CompositeDisposable();
 
     protected void init(final int layout, final Class<? extends BaseActivity> targetActivity, final String heading, final String text) {
+        /**
+         * Set up layout & FAB.
+         */
         setContentView(layout);
         setSupportActionBar(findViewById(R.id.toolbar));
 
@@ -40,40 +42,44 @@ public abstract class BaseActivity extends AppCompatActivity {
     @Override
     public void onResume() {
         super.onResume();
-        internetStatus = new InternetStatus(getApplicationContext());
         if (compositeDisposable == null || compositeDisposable.isDisposed()) {
             compositeDisposable = new CompositeDisposable();
         }
 
-        compositeDisposable.add(toastWhenNoInternet(getApplicationContext()));
-        compositeDisposable.add(internetStatus.pollWhileInternetAvailable()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(hasInternet -> {
-                    if (hasInternet) {
-                        final String still_has_internet = String.format("Still has internet? %s", hasInternet);
-                        Toast.makeText(getApplicationContext(), still_has_internet, Toast.LENGTH_SHORT).show();
-                    }
-                }, System.err::println)
+        final Observable<Boolean> hasInternet = createInternetStatusObservable();
 
-        );
+        final Disposable toastWhenHasInternet = hasInternet.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(this::toastIfYes, System.err::println);
+        final Disposable toastWhenNoInternet = hasInternet.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(this::toastIfNo, System.err::println);
+
+        compositeDisposable.add(toastWhenHasInternet);
+        compositeDisposable.add(toastWhenNoInternet);
+    }
+
+    private Observable<Boolean> createInternetStatusObservable() {
+        final InternetObservingSettings settings = InternetObservingSettings
+                .builder()
+                .host("www.example.com")
+                .strategy(new SocketInternetObservingStrategy())
+                .build();
+
+        return ReactiveNetwork.observeNetworkConnectivity(getApplicationContext()).switchMapSingle(connectivity -> ReactiveNetwork.checkInternetConnectivity(settings));
+    }
+
+    private void toastIfNo(final boolean hasInternet) {
+        if (!hasInternet) {
+            Toast.makeText(getApplicationContext(), "No internet connection", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void toastIfYes(final boolean hasInternet) {
+        if (hasInternet) {
+            Toast.makeText(getApplicationContext(), "Has internet connection", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
     public void onPause() {
         super.onPause();
         compositeDisposable.dispose();
-        internetStatus = null;
-    }
-
-    private Disposable toastWhenNoInternet(@NonNull final Context context) {
-        return  internetStatus.isAvailable()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(hasInternet -> {
-                    if (!hasInternet) {
-                        Toast.makeText(context, "No internet connection", Toast.LENGTH_SHORT).show();
-                    }
-                }, System.err::println);
     }
 }
