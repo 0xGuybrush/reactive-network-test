28ccc13038b1dc3cfd20d29d05c82c27f4b57f2d
Dave Moloney <dave@native.io>
2020-04-07 14:31:15 +0100

    Remove excessive debugging

--- app/src/main/java/ie/davidmoloney/reactivenetworktest/BaseActivity.java
+++ app/src/main/java/ie/davidmoloney/reactivenetworktest/BaseActivity.java
@@ -26,39 +26,15 @@ public abstract class BaseActivity extends AppCompatActivity {
 
     protected void init(final int layout, final Class<? extends BaseActivity> targetActivity, final String heading, final String text) {
         setContentView(layout);
-        final Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setSupportActionBar(findViewById(R.id.toolbar));
 
-        final FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(view -> {
+        this.<FloatingActionButton>findViewById(R.id.fab).setOnClickListener(view -> {
             Snackbar.make(view, heading, Snackbar.LENGTH_LONG).setAction(text, v -> {
                 Intent intent = new Intent(this, targetActivity);
                 startActivity(intent);
                 finish();
             }).show();
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -75,7 +51,7 @@ public abstract class BaseActivity extends AppCompatActivity {
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(hasInternet -> {
                     if (hasInternet) {
-                        final String still_has_internet = String.format("Still has internet. %s", hasInternet);
+                        final String still_has_internet = String.format("Still has internet? %s", hasInternet);
                         Toast.makeText(getApplicationContext(), still_has_internet, Toast.LENGTH_SHORT).show();
                     }
                 }, System.err::println)
--- app/src/main/java/ie/davidmoloney/reactivenetworktest/InternetStatus.java
+++ app/src/main/java/ie/davidmoloney/reactivenetworktest/InternetStatus.java
@@ -11,55 +11,38 @@ import java.util.concurrent.TimeUnit;
 import io.reactivex.Observable;
 import io.reactivex.schedulers.Schedulers;
 
-public class InternetStatus {
+class InternetStatus {
 
     private final Observable<Boolean> isAvailable;
 
-    public InternetStatus(final Context context) {
-        final InternetObservingSettings settings = InternetObservingSettings.builder()
-                .host("www.example.com")
-                .strategy(new SocketInternetObservingStrategy())
-                .build();
+    InternetStatus(final Context context) {
+        final InternetObservingSettings settings = InternetObservingSettings
+                                                        .builder()
+                                                        .host("www.example.com")
+                                                        .strategy(new SocketInternetObservingStrategy())
+                                                        .build();
 
         final Context applicationContext = context.getApplicationContext();
-        isAvailable = ReactiveNetwork.observeNetworkConnectivity(applicationContext)
-                .doOnSubscribe(subscription -> System.out.println(String.format("DEBUG::: ReactiveNetwork.observeNetworkConnectivity - subscribed. :%s", subscription)))
-                .doOnError(error -> System.out.println(String.format("DEBUG::: ReactiveNetwork.observeNetworkConnectivity - errored. %s", error.getMessage())))
-                .doOnComplete(() -> System.out.println("DEBUG::: ReactiveNetwork.observeNetworkConnectivity - completed"))
-                .doOnTerminate(() -> System.out.println("DEBUG::: ReactiveNetwork.observeNetworkConnectivity - terminated"))
-                .switchMapSingle(connectivity -> ReactiveNetwork.checkInternetConnectivity(settings))
-                .subscribeOn(Schedulers.io())
-                .doOnSubscribe(subscription -> System.out.println(String.format("DEBUG::: ReactiveNetwork.checkInternetConnectivity - subscribed. :%s", subscription)))
-                .doOnError(error -> System.out.println(String.format("DEBUG::: ReactiveNetwork.checkInternetConnectivity - errored. %s", error.getMessage())))
-                .doOnComplete(() -> System.out.println("DEBUG::: ReactiveNetwork.checkInternetConnectivity - completed"))
-                .doOnTerminate(() -> System.out.println("DEBUG::: ReactiveNetwork.checkInternetConnectivity - terminated"))
-                .distinctUntilChanged()
-                .doOnSubscribe(subscription -> System.out.println(String.format("DEBUG::: Subscribed to internet status. :%s", subscription)))
-                .doOnNext(hasInternet -> {
-                    System.out.println(String.format("DEBUG::: Internet status :%s", hasInternet));
-                })
-                .doOnComplete(() -> System.out.println("DEBUG::: InternetStatus completed"))
-                .doOnError(error -> System.out.println(String.format("DEBUG::: InternetStatus errored %s", error.getMessage())))
-                .doOnTerminate(() -> System.out.println("DEBUG::: InternetStatus terminated"));
+
+        isAvailable = ReactiveNetwork
+                        .observeNetworkConnectivity(applicationContext)
+                        .switchMapSingle(connectivity -> ReactiveNetwork.checkInternetConnectivity(settings))
+                        .subscribeOn(Schedulers.io())
+                        .distinctUntilChanged();
     }
 
-    public Observable<Boolean> pollWhileInternetAvailable() {
+    Observable<Boolean> pollWhileInternetAvailable() {
         final long noInternetIndex = -1L;
 
         return Observable.combineLatest(
-                Observable.interval(0, 5, TimeUnit.SECONDS), isAvailable,
-                (counter, hasInternet) -> hasInternet ? counter : noInternetIndex
-        )
+                    Observable.interval(0, 5, TimeUnit.SECONDS), isAvailable,
+                    (counter, hasInternet) -> hasInternet ? counter : noInternetIndex
+                )
                 .distinctUntilChanged()
-                .map(counter -> counter > noInternetIndex)
-                .doOnSubscribe(subscription -> System.out.println(String.format("DEBUG::: Subscribed to InternetStatus.pollWhileInternetAvailable. :%s", subscription)))
-                .doOnNext(next -> System.out.println(String.format("DEBUG::: InternetStatus.pollWhileInternetAvailable :%s", next)))
-                .doOnComplete(() -> System.out.println("DEBUG::: InternetStatus.pollWhileInternetAvailable completed"))
-                .doOnError(error -> System.out.println(String.format("DEBUG::: InternetStatus.pollWhileInternetAvailable errored, %s", error.getMessage())))
-                .doOnTerminate(() -> System.out.println("DEBUG::: InternetStatus.pollWhileInternetAvailable terminated"));
+                .map(counter -> counter > noInternetIndex);
     }
 
-    public Observable<Boolean> isAvailable() {
+    Observable<Boolean> isAvailable() {
         return isAvailable;
     }
 }
